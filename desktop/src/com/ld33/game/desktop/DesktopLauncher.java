2d29931d531b1b9a5b985100dda57c6f821b1b35
Wong Cho Ching(Sadale) <chwong249664@yahoo.com>
2015-08-22 23:09:23 +0800

    Fix the window config of jar version

--- desktop/src/com/ld33/game/desktop/DesktopLauncher.java
+++ desktop/src/com/ld33/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.ld33.game.LD33Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 960;
+		config.height = 500;
+		config.resizable = false;
 		new LwjglApplication(new LD33Game(), config);
 	}
 }
